09ba15986de3ef13249c5177ae92916588eaae0a
tylermachado <3586466+tylermachado@users.noreply.github.com>
2026-02-20 17:08:16 -0500

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,15 +6,15 @@
 
 <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
   {#each filteredBreeds as breed}
-  <a 
-    href="/cat/{breed.id}"
-    >
-    <Card
-      image={breed.id}
-      title={breed.name}
-      subtitle={breed.temperament}
-      description={breed.description}>
-    </Card>
-</a>
+    <a 
+      href="/cat/{breed.id}"
+      >
+      <Card
+        image={breed.id}
+        title={breed.name}
+        subtitle={breed.temperament}
+        description={breed.description}>
+      </Card>
+    </a>
   {/each}
 </ul>
